ecd031e4abf567e6efe83d7462fea6ea4b5bda90
Matias Peñas <mattff3@gmail.com>
2020-10-21 01:30:49 -0300

    One way of consuming data from provider

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-
+    static contextType = ThemeContext;
+    
     render() { 
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return ( 
-            <div className="book-list">
+            <div className="book-list" style={{ color:theme.syntax, background: theme.bg }}>
                 <ul>
-                    <li> the way of kings</li>
-                    <li> the way of wind</li>
-                    <li> the way of das</li>
+                    <li style={{ background: theme.ui }}> the way of kings</li>
+                    <li style={{ background: theme.ui }}> the way of wind</li>
+                    <li style={{ background: theme.ui }}> the way of das</li>
                 </ul>
             </div>
          );

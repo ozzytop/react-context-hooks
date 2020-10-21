15a4f8813f717f5103d4ff620fff834b99a90522
Matias Peñas <mattff3@gmail.com>
2020-10-20 22:26:04 -0300

    First commit

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+
+    render() { 
+        return ( 
+            <div className="book-list">
+                <ul>
+                    <li> the way of kings</li>
+                    <li> the way of wind</li>
+                    <li> the way of das</li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default BookList;

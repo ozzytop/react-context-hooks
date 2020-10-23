991b49454676817ee43a07c1231b461f254524f1
Matias Peñas <mattff3@gmail.com>
2020-10-23 00:26:25 -0300

    Using Context in a functional component

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## React Context and Hooks
 
 Checking React Context and Hooks to implement in other project
+
+## Tutorial
+
+https://www.youtube.com/watch?v=6RhOzQciVwI&list=PL4cUxeGkcC9hNokByJilPg5g9m2APUePI&index=1
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-
-
+/*
 class BookList extends Component {
     
     // First method to consume the provider
@@ -22,5 +21,22 @@ class BookList extends Component {
          );
     }
 }
+*/
+
+const BookList = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+        <div className="book-list" style={{ color:theme.syntax, background: theme.bg }}>
+            <ul>
+                <li style={{ background: theme.ui }}> the way of kings</li>
+                <li style={{ background: theme.ui }}> the way of wind</li>
+                <li style={{ background: theme.ui }}> the way of das</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default BookList;
  
 export default BookList;
